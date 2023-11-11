165e7d8c2974e32b6ec8ebe1d315f9bcfe5ed876
Kugelspitzer <146951498+Kugelcord@users.noreply.github.com>
2023-11-11 01:41:06 +0100

    Add files via upload

---
+++ legend_radar/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+-- Definiere die Ressource
+name 'deine_ressourcen_bezeichnung'
+description 'Beschreibung deiner Ressource'
+author 'Dein Name'
+
+-- Definiere die Client-Skripte
+client_scripts {
+    'client.lua'
+}
+
+
+
+ server_scripts {
+    'legend_system.lua'
+}
